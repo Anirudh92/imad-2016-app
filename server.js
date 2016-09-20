69fddb11cf4b0b7aeae3fcf4b6cea120da3c64b1
Anirudh Anand <anianand92@gmail.com>
2016-09-20 21:08:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,74 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var ArticleOne = {
+    
+    title: 'Article One | Anirudh Anand',
+    heading: 'Article One',
+    date: '20th September 2016',
+    content: `<p>
+                The is Article One which presents the first content that can be used to read about something.The is Article One which presents the first content that can be used to read about something. The is Article One which presents the forst content that can be used to read about something. The is Article One which presents the forst content that can be used to read about something.
+            </p>
+            
+            <p>    
+                The is Article One which presents the first content that can be used to read about something.The is Article One which presents the first content that can be used to read about something. The is Article One which presents the forst content that can be used to read about something. The is Article One which presents the forst content that can be used to read about something.
+            </p>
+            
+            <p>    
+                The is Article One which presents the first content that can be used to read about something.The is Article One which presents the first content that can be used to read about something. The is Article One which presents the forst content that can be used to read about something. The is Article One which presents the forst content that can be used to read about something.
+            </p>`
+    
+};
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+
+var HtmlTemplate = `
+    <html>
+    <head>
+    <title>
+        ${title}    
+    </title>
+    <meta name="viewport" content="width-device-width, initial-scale=1"/>
+   <link href="ui/style.css" rel="stylesheet"/>
+    </head>
+<body>
+    <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        
+        <hr/>
+        
+        <h3>
+            ${heading}      
+        </h3>
+        
+        <div>
+            ${date}    
+        </div>
+        
+        <div>
+            
+            ${content}
+            
+        </div>
+    </div>
+</body>
+</html>`;
+    return HtmlTemplate;
+    
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+   res.send(createTemplate(ArticleOne)); 
 });
 
 app.get('/article-two',function(req,res){
