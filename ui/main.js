3c457eed70089a42f2dbb2d016120b86ed553640
Anirudh Anand <anianand92@gmail.com>
2016-09-28 12:22:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,14 +7,25 @@ element.innerHTML = 'new Value';
 
 var image = document.getElementById("madi");
 var marginLeft = 0;
-
+var marginRight = 0;
 function moveRight(){
     marginLeft = marginLeft + 1;
-    image.style.marginLeft = marginLeft + 'px';
+    if(marginLeft <=200)
+    {
+        image.style.marginLeft = marginLeft + 'px';
+    }
+        return 0;    
+    }
+
+function moveLeft(){
+    marginRight = marginRight + 1;
+    image.style.marginRight = marginRight + 'px';
 }
 
 image.onclick = function(){
     
     var interval = setInterval(moveRight,50);
+    
+    var interval1 = setInterval(moveLeft,50);
 
 }
