8b1bc3787531bc3611f7160fcf4aceb3077304c7
Anirudh Anand <anianand92@gmail.com>
2016-09-28 11:52:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,14 @@ console.log('Loaded!');
 
 var element = document.getElementById("main-text");
 element.innerHTML = 'new Value';
+
+// Move image
+
+var image = document.getElementById("madi");
+
+image.onclick = function(){
+    
+    image.style.marginLeft = '100px';
+
+    
+}
