35d433a9567cfa03b437bf539426157ccf602f48
Anirudh Anand <anianand92@gmail.com>
2016-10-20 18:11:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,16 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
     
 });
-
+var names = [];
+app.get('/submit-name/:name',function(req,res){
+    //get the name from the request object
+    var name = req.params.name;
+    
+    names.push(name);//TODO
+    //JSON
+    res.send(JSON.stringify(names));
+    
+});
 app.get('/:articleName',function(req,res){
     
    var articleName = req.params.articleName;
