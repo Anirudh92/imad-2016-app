edb14d9b8d36f48530233aba4ac8dc74bbe77301
Anirudh Anand <anianand92@gmail.com>
2016-10-20 21:58:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,9 +41,9 @@ submit.onclick = function(){
                 names = JSON.parse(names);
                 var list = '';
                 for(var i=0; i< names.length; i++){
-                    list = '<li>' +names[i]+ '</li>';
+                    list += '<li>' + names[i] + '</li>';
                 }
-                var ul = getElementById('namelist');
+                var ul = document.getElementById('namelist');
                 ul.innerHTML = list;
           }
       }
